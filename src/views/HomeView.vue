1560db7709f59ba72c7f1d60df4114053df4b2d1
Mansurkhan <mansurbekeshpulatov@gmail.com>
2025-09-06 19:10:34 +0500

    add (component) ToDoItem and js codes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup>
+import ToDoItem from "@/components/ToDoItem.vue";
+import {reactive, ref} from "vue";
+const todo = ref('')
+const todoList = reactive([])
 
+const add = () => {
+    todoList.unshift(todo.value)
+    todo.value = ''
+}
+
+const remove = (index) => {
+    todoList.splice(index, 1)
+}
 </script>
 
 <template>
@@ -9,6 +21,7 @@
                 <input v-model="todo" class="w-full border-2 border-blue-500 rounded-md p-2 outline-none" type="text" placeholder="type here...">
                 <button @click="add" class="bg-blue-500 text-white rounded-md px-4 py-2.5 hover:cursor-pointer">Add</button>
             </div>
+            <ToDoItem v-for="(item, index) in todoList" :key="index" :text="item" :index @delete-item="remove(index)" />
         </div>
     </main>
 </template>
