fd19cfaacae0d7bbc8eac0a10b14b745a152cb38
Mansurkhan <mansurbekeshpulatov@gmail.com>
2025-09-06 19:09:38 +0500

    add input and button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="bg-blue-500 h-200 py-20">
+        <div class="max-w-[470px] mx-auto bg-white p-6 rounded-lg">
+            <div class="w-full flex items-center justify-between gap-4 mb-8">
+                <input v-model="todo" class="w-full border-2 border-blue-500 rounded-md p-2 outline-none" type="text" placeholder="type here...">
+                <button @click="add" class="bg-blue-500 text-white rounded-md px-4 py-2.5 hover:cursor-pointer">Add</button>
+            </div>
+        </div>
+    </main>
 </template>
